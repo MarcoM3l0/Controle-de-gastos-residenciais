ee87371a2e9833738d202216bf0e5df4c0d6d1f3
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-12-15 17:06:56 -0300

    fix: alterar propriedade Descricao para Nome na classe Pessoa

--- Residencial.GastosAPI/Residencial.GastosAPI/Models/Pessoa.cs
+++ Residencial.GastosAPI/Residencial.GastosAPI/Models/Pessoa.cs
@@ -3,7 +3,7 @@
 public class Pessoa
 {
     public int PessoaId { get; set; }
-    public string? Descricao { get; set; }
+    public string? Nome { get; set; }
     public int Idade { get; set; }
 
     // Relacionamento 1:N com Transacao
