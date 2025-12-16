0f54c6caf1f1eb24c581a9ac7d16bf2da8471df0
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-12-15 21:45:01 -0300

    chore: registrar repositórios no container de injeção de dependência
    - Adicionados serviços Scoped para IPessoaRepository, ICategoriaRepository e ITransacaoRepository
    - Configuração garante que cada requisição receba uma instância própria dos repositórios
    - Facilita o uso do padrão Repository com suporte ao ciclo de vida adequado no ASP.NET Core

--- Residencial.GastosAPI/Residencial.GastosAPI/Program.cs
+++ Residencial.GastosAPI/Residencial.GastosAPI/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Residencial.GastosAPI.Context;
 using Residencial.GastosAPI.DTOs.Mappings;
+using Residencial.GastosAPI.Repositories.Concrete;
+using Residencial.GastosAPI.Repositories.Interfaces;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,10 @@ builder.Services.AddControllers()
     });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPessoaRepository, PessoaRepository>();
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<ITransacaoRepository, TransacaoRepository>();
 
 string mensagem = "A string de conexão 'DefaultConnection' não foi encontrada ou está inválida.";
 
